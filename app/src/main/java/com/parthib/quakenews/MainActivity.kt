fba495f150012d93b3f0b507ff6f44683279008d
lucy2512 <mail4parthib@gmail.com>
2021-08-06 21:52:43 +0530

    Testing with Sample Data

--- app/src/main/java/com/parthib/quakenews/MainActivity.kt
+++ app/src/main/java/com/parthib/quakenews/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.parthib.quakenews
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val earthquakes: ArrayList<String> = ArrayList()
+        earthquakes.add("San Francisco")
+        earthquakes.add("London")
+        earthquakes.add("Tokyo")
+        earthquakes.add("Mexico City")
+        earthquakes.add("Moscow")
+        earthquakes.add("Rio de Janeiro")
+        earthquakes.add("Paris")
+
+        // Find a reference to the {@link ListView} in the layout
+        val earthquakeListView: ListView = findViewById<View>(R.id.list) as ListView
+
+        // Create a new {@link ArrayAdapter} of earthquakes
+        val adapter = ArrayAdapter(
+               this,android.R.layout.simple_list_item_1,earthquakes)
+
+        // Set the adapter on the {@link ListView}
+        // so the list can be populated in the user interface
+
+        // Set the adapter on the {@link ListView}
+        // so the list can be populated in the user interface
+        earthquakeListView.setAdapter(adapter)
     }
 }
